c99a807d2642b2ed4883c778e219b27f4151bd5e
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-01-26 14:21:13 -0800

    Remove unused macros

--- config.hpp
+++ config.hpp
@@ -2,14 +2,8 @@
 #define PORT 15635
 #define BACKLOG 5
 
-#define MAX_CONNECTIONS 10
-#define MAX_REQUESTS 100
-
 // Request
 #define REQ_BUF_SIZE 65536
-
-// Response
-#define TIMEOUT 5
 
 // Macros
 #define DEBUG 0
